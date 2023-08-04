bf2da2989c223369761994b168a6fe020b434948
irsaajma1 <irsaajmal1234@gmail.com>
2023-08-04 16:24:40 -0700

    some changes in hero.jsx

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Hero.css'; // You can create a CSS file for styling
+
+const Hero = () => {
+    return (
+        <div className="hero">
+            <div className="hero-image"></div>
+            <div className="hero-text">
+                <p>The world's largest chain of hotels is Best Western</p>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
