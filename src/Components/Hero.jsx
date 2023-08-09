f57b6c1f9a743a1e77a8a47655cb516345ba3b7d
irsaajma1 <irsaajmal1234@gmail.com>
2023-08-09 13:13:14 -0700

    Hero Section Complete

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Hero.css'; // You can create a CSS file for styling
+import Explore from './Explore';
+import Partner from './Partner';
+
 
 const Hero = () => {
     return (
@@ -8,6 +11,8 @@ const Hero = () => {
                 <div className="hero-text">
                     <p className="Heading-1">The world's largest chain of hotels is Best Western</p>
                     <p className="description-1"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat elit sed pretium, egestas sed sit orem ipsum dolor sit amet,</p>
+                     <Explore></Explore>
+                    <Partner></Partner>
                 </div>
                 <div className="hero-image"></div>
             </div>
